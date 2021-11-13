33c3a6eab21ddb485efdf9ee3adf2a717da83bc2
mohaymen_rafi <mohaymen.rafi@gmail.com>
2021-11-13 16:50:51 +0600

    get admin

--- index.js
+++ index.js
@@ -192,6 +192,18 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.json(result);
     });
+
+    // get admin
+    app.get('/users/:email', async (req, res) => {
+      const { email } = req.params;
+      const query = { email };
+      const user = await usersCollection.find(query);
+      let isAdmin = false;
+      if (user?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.send({ admin: isAdmin });
+    });
   } finally {
     // await client.close()
   }
